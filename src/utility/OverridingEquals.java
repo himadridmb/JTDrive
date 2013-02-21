d12bbc42f22c878efc565648394a53b887ca83c0
Himadri Talukder <himadri@digitalmenubox.com>
2013-02-21 16:41:29 -0500

    Maven support + Override equal starts

---
+++ src/utility/OverridingEquals.java
@@ -0,0 +1,102 @@
+package utility;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class OverridingEquals {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Employee e1 = new Employee();
+		Employee e2 = new Employee();
+
+		e1.setId(100);
+		e2.setId(100);
+		// Prints false in console
+		System.out.println(e1.equals(e2));
+		
+		Set<Employee> employees = new HashSet<Employee>();
+        employees.add(e1);
+        employees.add(e2);
+        //Prints two objects
+        System.out.println(employees);
+	}
+
+}
+
+class Employee {
+	private Integer id;
+	private String firstname;
+	private String lastName;
+	private String department;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + "]";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) {
+			return false;
+		}
+		if (o == this) {
+			return true;
+		}
+		if (getClass() != o.getClass()) {
+			return false;
+		}
+		Employee e = (Employee) o;
+		return (this.getId() == e.getId());
+	}
+	
+	@Override
+	 public int hashCode()
+	 {
+	    final int PRIME = 31;
+	    int result = 1;
+	    result = PRIME * result + getId();
+	    return result;
+	 }
+}
+
+/*You could do "git pull origin master" to tell git the explicit branch you want to pull or you add this to your .git/config:
+
+[branch "master"]
+  remote = origin
+  merge = refs/heads/master
+*/
